f186b6e499ea9a586929c9c37df853d32e0c7683
Malin Elise Martinsen <malinelisemartinsen@Malins-MBP.home>
2022-02-22 16:25:09 +0100

    add all components

---
+++ README.md
@@ -0,0 +1,11 @@
+# Fitness Diary
+
+Compose diary posts through a "secret" compose site, and keep a track of and share the fitness journey.
+
+## Language
+
+This application is written with the framework Node.js and Ejs (JavaScript), with the use of HMTL and CSS to display the website in a more esthetic way. With use of Mongoose (MongoDB Atlas) as databaseserver.
+
+## Meaning behind the website
+
+The meaning behind the website is for the user to post blogposts (that's stored in a database) to evaluate and look at the fitnessprogress. There's an about- and contact page, where people can read about the person posting the posts, as well as contact for questions. The blog has a "hidden" compose-site, since it's only supposed to be accessible for the author.
---
+++ app.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const homeStartingContent =
+  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent =
+  "After a lot of thinking, I've finally decided to take the step - make a change in my lifestyle and follow my wish for a better and healthier body and mind. My fitness journy is something I want to share, in order to inspire others to take the step themselves, and also to give myself an incentive to keep it going. I've never been what the society thinks of as fat, but neither have I felt comfortable in my own body. As the media presents it, your body size should be within an ideal standard set by the minority. It's not though, the pressure from all around that makes me want to take this step - it comes from inside. Please don't hesitate to contact me if you have any questions.";
+const contactContent = "You can contact me through various channels:";
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose.connect(
+  "link to connection to database" // Due to security reasons, I've taken away my own mongoDB Atlas link.
+);
+
+const postSchema = {
+  title: String,
+  content: String,
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", function (req, res) {
+  Post.find({}, function (err, posts) {
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts,
+    });
+  });
+});
+
+app.get("/compose", function (req, res) {
+  res.render("compose");
+});
+
+app.post("/compose", function (req, res) {
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody,
+  });
+
+  post.save(function (err) {
+    if (!err) {
+      res.redirect("/");
+    }
+  });
+});
+
+app.get("/posts/:postId", function (req, res) {
+  const requestedPostId = req.params.postId;
+
+  Post.findOne({ _id: requestedPostId }, function (err, post) {
+    res.render("post", {
+      title: post.title,
+      content: post.content,
+    });
+  });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent: aboutContent });
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactContent });
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
